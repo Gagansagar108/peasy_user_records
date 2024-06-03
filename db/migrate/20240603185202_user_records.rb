e935e9b19544cecead007bcff7bd35b0322011f6
Gagansagar108 <79242532+Gagansagar108@users.noreply.github.com>
2024-06-04 00:37:07 +0530

    migration

--- db/migrate/20240603185202_user_records.rb
+++ db/migrate/20240603185202_user_records.rb
@@ -1,4 +1,24 @@
 class UserRecords < ActiveRecord::Migration[7.0]
   def change
+    create_table :users, id: :uuid, default: -> { 'gen_random_uuid()' }, do |t|
+      t.string :uuid
+      t.string :gender
+      t.name :jsonb
+      t.location :jsonb
+      t.integer :age
+      t.timestamps
+    end
+
+    create_table :user_records, id: :uuid, default: -> { 'gen_random_uuid()' }, do |t|
+      t.date :date
+      t.integer :male_count
+      t.integer :female_count
+      t.float :male_avg_age
+      t.float :female_avg_age
+      t.timestamps
+    end 
+
+    add_index :users, :uuid, unique: true
+    add_index :user_records, :date
   end
 end
