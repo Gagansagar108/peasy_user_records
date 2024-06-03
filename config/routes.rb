dc8e8370f14e9350759d6bd492d698a4b9716064
Gagansagar108 <79242532+Gagansagar108@users.noreply.github.com>
2024-06-04 00:52:07 +0530

    context path

--- config/routes.rb
+++ config/routes.rb
@@ -3,4 +3,10 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
   # root "articles#index"
+
+  scope '/peasy' do
+    resources :users
+    resources :user_records
+    root to: 'home#index'
+  end
 end
