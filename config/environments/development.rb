0aefec5ef259ced0153e335dd8ac6320cb3c7139
Gagansagar108 <79242532+Gagansagar108@users.noreply.github.com>
2024-06-04 11:46:02 +0530

    minor

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -68,17 +68,17 @@ Rails.application.configure do
   # Uncomment if you wish to allow Action Cable access from any origin.
   # config.action_cable.disable_request_forgery_protection = true
 
-  Rails.application.configure do
-    # Other configuration options...
+#   Rails.application.configure do
+#     # Other configuration options...
   
-    config.hosts << "the-real-gng.online"
-  end
+#     config.hosts << "the-real-gng.online"
+#   end
 
-  config.cache_store = :redis_store, {
-  host: 'localhost',
-  port: 6379,
-  db: 3,
-  namespace: 'cache'
-}
+#   config.cache_store = :redis_store, {
+#   host: 'localhost',
+#   port: 6379,
+#   db: 3,
+#   namespace: 'cache'
+# }
 
 end
