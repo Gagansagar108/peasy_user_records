a26e283d14a50611dc48a92b7eae91d9295112dd
Gagansagar108 <79242532+Gagansagar108@users.noreply.github.com>
2024-06-04 21:47:29 +0530

    cache

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -56,6 +56,8 @@ Rails.application.configure do
   # Highlight code that triggered database queries in logs.
   config.active_record.verbose_query_logs = true
 
+  config.cache_store = :redis_store, ‘redis://localhost:6379/0, { expires_in: 1.days }
+
   # Suppress logger output for asset requests.
   config.assets.quiet = true
 
