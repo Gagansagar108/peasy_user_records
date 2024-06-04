0fdaa84c6f4467928c775ed3e32931d95907cffe
Gagansagar108 <79242532+Gagansagar108@users.noreply.github.com>
2024-06-05 01:34:45 +0530

    minor

--- app/workers/test_job.rb
+++ app/workers/test_job.rb
@@ -4,9 +4,8 @@ class TestJob
   
     def perform(args = {})
       # Your background job logic goes here
-      User.create!(uuid: Randon.hex, name: {name: "#{Random.alphanumeric}gagan"} )
+      User.create!(uuid: Random.hex, name: {name: "#{Random.alphanumeric}gagan"} )
   
-      # Example: Send an email notification
-      # UserMailer.with(user: User.find(args[:user_id])).welcome_email.deliver_now
+    
     end
   end
