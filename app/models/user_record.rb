b18151e90688e17c16e314bc1c8c5d1aea33e77b
Gagansagar108 <79242532+Gagansagar108@users.noreply.github.com>
2024-06-05 22:44:03 +0530

    minot

--- app/models/user_record.rb
+++ app/models/user_record.rb
@@ -1,12 +1,11 @@
 class UserRecord < ApplicationRecord
     has_many :users, class_name: :User, primary_key: :date_of_entry, foreign_key: :date_of_entry
-    binding.pry
+
     include ActiveModel::Dirty
     define_attribute_methods 
-    after_save :do_changes
+    #after_save :do_changes
 
     def do_changes
-        binding.pry
         return unless changed?
         query = "SELECT gender, AVG(age) from users where age is not null GROUP by gender"
         data = ActiveRecord::Base.connection.execute(query).as_json.map(&:deep_symbolize_keys)
