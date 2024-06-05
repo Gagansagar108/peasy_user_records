a16213e996d91697da5c6b1ee37c94906bf42997
Gagansagar108 <79242532+Gagansagar108@users.noreply.github.com>
2024-06-05 22:56:54 +0530

    t

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,6 +2,7 @@ class UsersController < ApplicationController
 
     def index
         @user = User.all
+        render json: {"e": 3344}
         render :inline => Liquid::Template.parse(File.read(Rails.root.join("app", "views","users", "dashboard.liquid"))).render({'products' => @user})
     end
 
