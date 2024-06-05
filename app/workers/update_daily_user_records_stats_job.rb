a2d91dfd22b555e830b2fdd4aa9192ba30810b31
Gagansagar108 <79242532+Gagansagar108@users.noreply.github.com>
2024-06-05 12:21:48 +0530

    minor

--- app/workers/update_daily_user_records_stats_job.rb
+++ app/workers/update_daily_user_records_stats_job.rb
@@ -3,8 +3,7 @@ class UpdateDailyUserRecordsStatsJob
     sidekiq_options queue: :default
   
     def perform(args = {})
-        date = Time.zone.now.to_date
-        daily_users = User.where("users.created_at   Wed, 05 Jun 2024")
+        
       end 
     end
 end 
