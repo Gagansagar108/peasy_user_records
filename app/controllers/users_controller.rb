5d5ae0d8ec39c94b9b8297ac3de65f03c46c2a34
Gagansagar108 <79242532+Gagansagar108@users.noreply.github.com>
2024-06-06 23:34:05 +0530

    erb

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,7 +2,7 @@ class UsersController < ApplicationController
 
     def index
         @user = User.all
-        template = Liquid::Template.parse(File.read(Rails.root.join("app", "views","users", "dashboard.liquid")))
+        template = Liquid::Template.parse(File.read(Rails.root.join("app", "views","users", "dashboard.liquid.erb")))
         render template.render({'users' => @user})
 
         render locals: { user: @user }
