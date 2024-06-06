6e01f30ea09f1dce7c1f718ef3ba889673bae04f
Gagansagar108 <79242532+Gagansagar108@users.noreply.github.com>
2024-06-06 23:47:50 +0530

    pry

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,6 +2,7 @@ class UsersController < ApplicationController
 
     def index
         @user = User.all
+        binding.pry
         template = Liquid::Template.parse(File.read(Rails.root.join("app", "views","users", "dashboard.liquid.erb")))
 
         render "users/show", locals: { user: @user }
