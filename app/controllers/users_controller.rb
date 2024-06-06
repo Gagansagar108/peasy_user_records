671afe46fa2c12848db25c8f9ecb1fd0057e0a2e
Gagansagar108 <79242532+Gagansagar108@users.noreply.github.com>
2024-06-07 01:14:26 +0530

    minor

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,7 +3,7 @@ class UsersController < ApplicationController
     def index
         @user = User.all
         binding.pry
-        @template = Liquid::Template.parse(File.read(Rails.root.join("app", "views","users", "dashboard.liquid"))).render({'users' => @user.as_json.map(&:deep_symbolize_keys) })
+        @template = Liquid::Template.parse(File.read(Rails.root.join("app", "views","users", "dashboard.liquid"))).render({'users' => @user.as_json })
 
         render inline:  @template
 
