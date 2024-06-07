bd8b24aa334c6c8e26d8c049cdf26d93847f29dd
Gagansagar108 <79242532+Gagansagar108@users.noreply.github.com>
2024-06-07 22:33:27 +0530

    query name

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,9 +1,12 @@
 class User < ApplicationRecord
     has_one :record, class_name: :UserRecord, primary_key: :date_of_entry, foreign_key: :date_of_entry
+     
+    before_validation :set_query_name
 
-    before_validation do
+    def set_query_name
+        binding.pry
         name = self.name
         location = self.location
         self.query_data = "#{name[:title]} #{name[:first]} #{name[:last]} #{location[:city]}".downcase
-      end
+    end 
 end
