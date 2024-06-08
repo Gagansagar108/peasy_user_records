57c7527731926726e335e5ed49ffd0eaf1e30a66
Gagansagar108 <79242532+Gagansagar108@users.noreply.github.com>
2024-06-08 14:41:45 +0530

    helper

---
+++ app/helpers/user_records_helper.rb
@@ -0,0 +1,21 @@
+module UserRecordsHelper
+    def get_gender_wise_users_count(params = {})
+        date_of_entry = params[:date_of_entry] || Time.zone.now.to_date
+      
+        daily_users = User.where(date_of_entry: date_of_entry)
+       
+        genders_count = daily_users.group(:gender).count
+       
+        daily_records_params = {date_of_entry: date_of_entry}
+       
+        daily_records_params.merge!({female_count: genders_count['female'], male_count: genders_count['male']})
+
+        create_daily_records(daily_records_params)
+    end
+
+    def create_daily_records(params)
+        user_record = UserRecord.find_or_initialize_by(date_of_entry: params[:date_of_entry])
+        user_record.assign_attributes(params)
+        user_record.save!
+    end 
+end 
--- app/workers/update_daily_user_records_stats_job.rb
+++ app/workers/update_daily_user_records_stats_job.rb
@@ -3,18 +3,6 @@ class UpdateDailyUserRecordsStatsJob
     sidekiq_options queue: :default
   
     def perform(args = {})
-        date_of_entry = Time.zone.now.to_date
-        daily_users = User.where(date_of_entry: date_of_entry)
-        genders_count = daily_users.group(:gender).count
-        params = {date_of_entry: date_of_entry}
-        params.merge!({female_count: genders_count['female'], male_count: genders_count['male']})
-
-        create_daily_records(params)
+        UserRecordsHelper::get_gender_wise_users_count()
     end
-
-    def create_daily_records(params)
-       user_record = UserRecord.find_or_initialize_by(date_of_entry: params[:date_of_entry])
-       user_record.assign_attributes(params)
-       user_record.save!
-    end 
 end 
