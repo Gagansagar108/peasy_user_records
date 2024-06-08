1fc1ac20fb4ab27bf97fef952c8719902dc6da09
Gagansagar108 <79242532+Gagansagar108@users.noreply.github.com>
2024-06-08 13:32:51 +0530

    minor

--- app/models/user_record.rb
+++ app/models/user_record.rb
@@ -7,11 +7,14 @@ class UserRecord < ApplicationRecord
     before_save :do_changes
 
     def do_changes
-        binding.pry
-        return unless changed?
+        return if (changes.keys & ['male_count', 'female_count']).blank?
+        
         query = "SELECT gender, AVG(age) from users where age is not null and date_of_entry = '#{self.date_of_entry}' GROUP by gender"
+      
         data = ActiveRecord::Base.connection.execute(query).as_json.map(&:deep_symbolize_keys)
+      
         params = data.inject({}){|params,y| params.merge!("#{y[:gender]}_avg_age": y[:avg])}
+      
         self.update!(params)
     end 
 end 
